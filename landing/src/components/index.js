3c5fe72aa061fb05f3e58e3226b485c82f286f70
heliosalesjr <heliosalesjr@gmail.com>
2023-05-28 10:48:11 -0300

    hero and stats

--- landing/src/components/index.js
+++ landing/src/components/index.js
@@ -1,5 +1,5 @@
 
-
+import Navbar from "./Navbar";
 import Billing from "./Billing";
 import CardDeal from "./CardDeal";
 import Business from "./Business";
@@ -9,7 +9,7 @@ import Stats from "./Stats";
 import Footer from "./Footer";
 import Testimonials from "./Testimonials";
 import Hero from "./Hero";
-import Navbar from "./Navbar";
+
 
 export {
   Navbar,
